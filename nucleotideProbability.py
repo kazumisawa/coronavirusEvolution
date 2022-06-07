a703edf33111f13453e1b6b973e3d202a813bef5
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2022-06-07 17:09:17 +0900

    Update nucleotideProbability.py

--- nucleotideProbability.py
+++ nucleotideProbability.py
@@ -1,1 +1,37 @@
+import numpy as np
 
+
+def Prob(a, h, T):
+    result = np.array( [a] * 16 ).reshape( (4,4) )
+    result[1,0] = h
+    result[0,0] = -2*a -h
+    result[1,1] = result[2,2] = result[3,3] = -3*a
+    return result
+
+
+nucleotide = list("CTGA")
+nucDict = dict()
+for i in range( len(nucleotide) ):
+    nucDict[ nucleotide[i] ] = i
+
+
+seq ="AATTGGCCCGG"
+seqLen = len(seq)
+
+ancestral = np.zeros( ( seqLen, len(nucleotide) ) )
+descendant = np.zeros( (seqLen, len(nucleotide) ) )
+
+for i in range(seqLen):
+    c = seq[i]
+    ancestral[ i, nucDict[c] ] =1
+
+#print(ancestral)
+
+P =  Prob(0.01,0.03,1)
+
+print(ancestral)
+
+
+for i in range(seqLen):
+    site = ancestral[i]
+    print( np.dot( P, site ) )
