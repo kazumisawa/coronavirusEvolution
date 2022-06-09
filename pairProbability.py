ef1846c251fceb5c7c1671822c65d79651f25192
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2022-06-09 09:31:01 +0900

    Update pairProbability.py

--- pairProbability.py
+++ pairProbability.py
@@ -1,1 +1,24 @@
 # calculate the probability from a pair of aligned sequenes
+# version 0.0 by Kazuharu Misawa 2022/06/09
+import sys
+import math
+import numpy as np
+import random
+import re
+import statistics
+from io import StringIO
+from Bio import AlignIO
+from Bio import SeqIO
+from Bio.Align.Applications import MafftCommandline
+
+for k in range(n):
+    #pairwizeAlignment
+    with open("temp.fas","w") as tmpfile:
+        SeqIO.write( referenceList[0], tmpfile, "fasta")
+        SeqIO.write( targetList[k], tmpfile, "fasta")
+    mafft_cline = MafftCommandline("mafft", input = "temp.fas")
+    output1, output2 = mafft_cline()
+    alignment = AlignIO.read(StringIO(output1), "fasta")
+#    pair = pairCount(alignment[0].seq, alignment[1].seq)
+    print( alignment )
+  
